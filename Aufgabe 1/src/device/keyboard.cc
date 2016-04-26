f608c52ab4978397fcf1633759d074a5f4a14249
Arne <sujomear@arcor.de>
2016-04-26 08:38:42 +0200

    added patch2

---
+++ Aufgabe 1/src/device/keyboard.cc
@@ -0,0 +1,26 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\
+ *                                   Technische Informatik II                                    * 
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+ *                                                                                               * 
+ *                                       K E Y B O A R D                                         * 
+ *                                                                                               * 
+\* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+/* * * * * * * * * * * * * * * * * * * * * * * * *\
+#                    INCLUDES                     #
+\* * * * * * * * * * * * * * * * * * * * * * * * */
+#include "device/keyboard.h"
+
+/* * * * * * * * * * * * * * * * * * * * * * * * *\
+#                    METHODS                      # 
+\* * * * * * * * * * * * * * * * * * * * * * * * */
+
+/** \todo implement **/
+void Keyboard::plugin(){
+    // ToDo: your code goes here
+}
+
+/** \todo implement **/
+void Keyboard::trigger(){
+    // ToDo: your code goes here
+}
