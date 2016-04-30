264c0b5231414a87e9a0050d9d0c98cae81963c4
Arne <sujomear@arcor.de>
2016-04-30 16:33:53 +0200

    am now master of assembler

--- Theoretische Aufgabe 1/ex/pointer.c
+++ Theoretische Aufgabe 1/ex/pointer.c
@@ -2,10 +2,14 @@
 int	main(){
   char	a;
   a=5;
+// get pointer to a
   char	*p	=	&a;
+// give value of p and value of address pointed to by p
   printf("p	address	%p\n",	p);
   printf("p	content	%d\n",	*p);
+// overwrite p with garbage
   *p=22;
+// a still exists
   printf("a	address	%p\n",	&a);
   printf("a	content	%d\n",	a);
 }
--- Theoretische Aufgabe 1/ex/pointer.s
+++ Theoretische Aufgabe 1/ex/pointer.s
@@ -14,44 +14,49 @@
 main:
 .LFB0:
 	.cfi_startproc
-	pushq	%rbp
-	.cfi_def_cfa_offset 16
+	pushq	%rbp		# save base pointer to stack
+	.cfi_def_cfa_offset 16	# .cfi_*: debug, exception handling
 	.cfi_offset 6, -16
-	movq	%rsp, %rbp
+	movq	%rsp, %rbp	# use current stack top as stack base for now
 	.cfi_def_cfa_register 6
-	subq	$16, %rsp
-	movb	$5, -9(%rbp)
-	leaq	-9(%rbp), %rax
-	movq	%rax, -8(%rbp)
-	movq	-8(%rbp), %rax
-	movq	%rax, %rsi
-	movl	$.LC0, %edi
-	movl	$0, %eax
-	call	printf
-	movq	-8(%rbp), %rax
-	movzbl	(%rax), %eax
-	movsbl	%al, %eax
-	movl	%eax, %esi
-	movl	$.LC1, %edi
-	movl	$0, %eax
-	call	printf
-	movq	-8(%rbp), %rax
-	movb	$22, (%rax)
-	leaq	-9(%rbp), %rax
-	movq	%rax, %rsi
-	movl	$.LC2, %edi
-	movl	$0, %eax
-	call	printf
-	movzbl	-9(%rbp), %eax
-	movsbl	%al, %eax
-	movl	%eax, %esi
-	movl	$.LC3, %edi
-	movl	$0, %eax
-	call	printf
-	movl	$0, %eax
-	leave
+	subq	$16, %rsp	# subtract 16 from top pointer to keep space for our variables free (increase frame size)
+	movb	$5, -9(%rbp)	# save 5 (a) into our stack frame
+	leaq	-9(%rbp), %rax	# save the address of the 5 (p:=&a) into rax
+	movq	%rax, -8(%rbp)	# move the address to stack frame (p)
+############################################################# ^char *p = &a ############################################################# ^
+	movq	-8(%rbp), %rax	# copy a's address from p (in the stack frame) to rax
+	movq	%rax, %rsi	# copy p(==a's address) to the second argument register
+	movl	$.LC0, %edi	# copy a reference to the string to the first argument register
+	movl	$0, %eax	# clean eax (why not rax?)
+	call	printf		# call printf
+############################################################# ^printf p #############################################################
+	movq	-8(%rbp), %rax	# copy p to rax
+	movzbl	(%rax), %eax	# move, padding with zeroes, from byte to long. copy rax to eax, padding with zeros
+	movsbl	%al, %eax	# move, padding with the src MSB, from byte to long. copy the lower 8 bits from rax to eax, 
+	movl	%eax, %esi	# copy *p==a to the second argument register
+	movl	$.LC1, %edi	# copy a reference to the string to the first argument register
+	movl	$0, %eax	# clear eax
+	call	printf		# call printf
+############################################################# ^printf *p #############################################################
+	movq	-8(%rbp), %rax	# copy p to rax
+	movb	$22, (%rax)	# overwrite rax with 22
+	leaq	-9(%rbp), %rax	# copy effective address of a (==&a) to rax
+	movq	%rax, %rsi	# copy &a==(old p) to the second argument register
+	movl	$.LC2, %edi	# copy a reference to the string to the first argument register
+	movl	$0, %eax	# clear eax
+	call	printf		# call printf
+############################################################# ^printf &a #############################################################
+	movzbl	-9(%rbp), %eax	# move from a to eax, padding from byte to long with zeroes
+	movsbl	%al, %eax	# copy from al to eax, this time padding with al's MSB (sign)
+	movl	%eax, %esi	# copy a to the second argument register
+	movl	$.LC3, %edi	# copy reference to the string to the first argument register
+	movl	$0, %eax	# clear eax
+	call	printf		# call printf
+############################################################# ^printf a #############################################################
+	movl	$0, %eax	# clear eax
+	leave			# leave the stack frame
 	.cfi_def_cfa 7, 8
-	ret
+	ret			# return from function
 	.cfi_endproc
 .LFE0:
 	.size	main, .-main
