ffdbe59f04a147ae0fa3f8b0c09d8fadb93531c6
Arne <sujomear@arcor.de>
2016-05-10 18:34:27 +0200

    patched to a3

---
+++ Aufgabe 3/src/common/panic.cc
@@ -0,0 +1,27 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\
+ *                                   Technische Informatik II                                    * 
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+ *                                                                                               * 
+ *                                          P A N I C                                            * 
+ *                                                                                               * 
+\* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+/* * * * * * * * * * * * * * * * * * * * * * * * *\
+#                    INCLUDES                     #
+\* * * * * * * * * * * * * * * * * * * * * * * * */
+#include <common/panic.h>
+#include <object/cpu.h>
+#include <object/kout.h>
+
+/* * * * * * * * * * * * * * * * * * * * * * * * *\
+#                    METHODS                      # 
+\* * * * * * * * * * * * * * * * * * * * * * * * */
+
+void Panic::trigger(){
+  kout << "Unhandled interrupt: '" << mCurrentInterrupt << "'" << endl;
+  cpu.halt();
+}
+
+void Panic::currentInterrupt(int iNum){
+  mCurrentInterrupt = iNum;
+}
